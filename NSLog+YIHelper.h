81e87a9573435e8703bb6965b3abfbef184645c8
Yasuhiro Inami <inamiy@gmail.com>
2012-12-03 16:55:46 +0900

    Modernize code, add LOG_RANGE, add #if defined(YI_DEBUG).

--- NSLog+YIHelper.h
+++ NSLog+YIHelper.h
@@ -6,13 +6,13 @@
 //  Copyright 2011 Yasuhiro Inami. All rights reserved.
 //
 
-#ifdef DEBUG
+#if defined(DEBUG) || defined(YI_DEBUG)
 
 #   define LOG(...)         NSLog(__VA_ARGS__)
 #   define LOG_LINE         NSLog(@"---------------");
 #   define LOG_BREAK        { for (int i = 0; i < 2; i++) NSLog(@" "); }
 #   define LOG_METHOD       NSLog(@"%s line %d", __func__, __LINE__)
-#   define LOG_RETAIN(obj)  NSLog(@"%s retainCount=%d", #obj, [obj retainCount]) 
+#   define LOG_RETAIN(obj)  NSLog(@"%s retainCount=%d", #obj, (int)CFGetRetainCount((__bridge CFTypeRef)obj))
 #   define LOG_OBJECT(obj)  NSLog(@"%s=%@", #obj, obj)
 #   define LOG_DATE(date)   NSLog(@"%s=%@", #date, [date descriptionWithLocale:[NSLocale currentLocale]])
 #   define LOG_INT(i)       NSLog(@"%s=%d", #i, i)
@@ -20,8 +20,9 @@
 #   define LOG_RECT(rect)   NSLog(@"%s x=%.2f, y=%.2f, w=%.2f, h=%.2f", #rect, rect.origin.x, rect.origin.y, rect.size.width, rect.size.height)
 #   define LOG_POINT(pt)    NSLog(@"%s x=%.2f, y=%.2f", #pt, pt.x, pt.y)
 #   define LOG_SIZE(size)   NSLog(@"%s w=%.2f, h=%.2f", #size, size.width, size.height)
-#   define LOG_TRANSFORM(transform)     NSLog(@"transform = %@", NSStringFromCGAffineTransform(transform));
-#   define LOG_TRANSFORM3D(transform3D) NSLog(@"transform3D =\n%f %f %f %f\n%f %f %f %f\n%f %f %f %f\n%f %f %f %f\n\n",transform3D.m11,transform3D.m12,transform3D.m13,transform3D.m14,transform3D.m21,transform3D.m22,transform3D.m23,transform3D.m24,transform3D.m31,transform3D.m32,transform3D.m33,transform3D.m34,transform3D.m41,transform3D.m42,transform3D.m43,transform3D.m44);
+#   define LOG_RANGE(range) NSLog(@"%s loc=%d, len=%d", #range, range.location, range.length)
+#   define LOG_TRANSFORM(transform)     NSLog(@"%s = %@", #transform, NSStringFromCGAffineTransform(transform));
+#   define LOG_TRANSFORM3D(transform3D) NSLog(@"%s =\n%f %f %f %f\n%f %f %f %f\n%f %f %f %f\n%f %f %f %f\n\n",#transform3D,transform3D.m11,transform3D.m12,transform3D.m13,transform3D.m14,transform3D.m21,transform3D.m22,transform3D.m23,transform3D.m24,transform3D.m31,transform3D.m32,transform3D.m33,transform3D.m34,transform3D.m41,transform3D.m42,transform3D.m43,transform3D.m44);
 
 #   define LOG_THREAD       NSLog(@"thread=%@", [NSThread currentThread])
 #   define LOG_STACK        NSLog(@"%@", [NSThread callStackSymbols])
@@ -47,6 +48,7 @@
 #   define LOG_RECT(rect)   ;
 #   define LOG_POINT(pt)    ;
 #   define LOG_SIZE(size)   ;
+#   define LOG_RANGE(range) ;
 #   define LOG_TRANSFORM(transform)     ;
 #   define LOG_TRANSFORM3D(transform3D) ;
 
@@ -59,4 +61,4 @@
 #   define LOG_START(name)  ;
 #   define LOG_STOP(name)   ;
 
-#endif // DEBUG
+#endif
