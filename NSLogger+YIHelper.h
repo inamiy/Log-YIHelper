b6ab37b06a20baf4d320568e9953a429c6b739ae
Yasuhiro Inami <inamiy@gmail.com>
2013-10-24 15:35:39 +0900

    Add semicolon terminator for all LOG macros.

--- NSLogger+YIHelper.h
+++ NSLogger+YIHelper.h
@@ -20,18 +20,18 @@
 
 #   import "LoggerClient.h"
 
-#   define LOGGER(tag,level,...)            LogMessageF  (__FILE__,__LINE__,__FUNCTION__,tag,level,__VA_ARGS__)
-#   define LOGGER_DATA(tag,level,data)      LogDataF     (__FILE__,__LINE__,__FUNCTION__,tag,level,data)
+#   define LOGGER(tag,level,...)            LogMessageF  (__FILE__,__LINE__,__FUNCTION__,tag,level,__VA_ARGS__);
+#   define LOGGER_DATA(tag,level,data)      LogDataF     (__FILE__,__LINE__,__FUNCTION__,tag,level,data);
 #   define LOGGER_IMAGE(tag,level,image) \
         if ([image isKindOfClass:[UIImage class]]) { \
             LogImageDataF(__FILE__,__LINE__,__FUNCTION__,tag,level,CGImageGetWidth(image.CGImage),CGImageGetHeight(image.CGImage),UIImagePNGRepresentation(image)); \
         }
-#   define LOGGER_MARK(text)                LogMarker(text)
+#   define LOGGER_MARK(text)                LogMarker(text);
 
-#   define LGR(...)                         LOGGER(nil,0,__VA_ARGS__)
-#   define LGR_DATA(data)                   LOGGER_DATA(nil,0,data)
-#   define LGR_IMAGE(image)                 LOGGER_IMAGE(nil,0,image)
-#   define LGR_MARK(text)                   LOGGER_MARK(text)
+#   define LGR(...)                         LOGGER(nil,0,__VA_ARGS__);
+#   define LGR_DATA(data)                   LOGGER_DATA(nil,0,data);
+#   define LGR_IMAGE(image)                 LOGGER_IMAGE(nil,0,image);
+#   define LGR_MARK(text)                   LOGGER_MARK(text);
 
 #else
 
